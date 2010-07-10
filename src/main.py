144642cee21352e7c9346158d087c344dec8d46f
Sweetwater <sweeeetwater@gmail.com>
2010-07-10 21:44:32 +0900

    チャネルによるイベント通知をポーリングに変更

--- src/main.py
+++ src/main.py
@@ -13,12 +13,19 @@ class UserData(db.Model):
     user = db.UserProperty(required=True)
     addtime = db.DateTimeProperty(auto_now_add=True)
 
+#ターン行動
+class TurnActionData(db.Model):
+    putPlayer = db.StringProperty(choices=['p1','p2'])
+    putIndex = db.IntegerProperty(default=-1)
+
 # 試合データ
 class MatchData(db.Model):
     status = db.StringProperty(choices=['start','p1_turn','p2_turn','end'], default='start')
     player1 = db.ReferenceProperty(UserData, collection_name='1player')
     player2 = db.ReferenceProperty(UserData, collection_name='2player')
     board = db.ListProperty(long, default=[0,0,0, 0,0,0, 0,0,0])
+    frame = db.IntegerProperty(default=0)
+    lastTurnAction = db.ReferenceProperty(TurnActionData)
 
 def PlayerPageGet(self, playerId):
     # ユーザがログインしているか調べる
@@ -31,8 +38,11 @@ def PlayerPageGet(self, playerId):
 
         # 試合データの初期化
         matchName = 'firstMatch'
+        turnActionName = 'lastTurnAction'
         matchData = MatchData.get_or_insert(matchName)
         matchData.status = 'p1_turn'
+        matchData.frame = 0
+        matchData.lastTurnAction = TurnActionData.get_or_insert(turnActionName)
 
         # 試合データにユーザを追加
         if playerId == 'p1':
@@ -44,11 +54,12 @@ def PlayerPageGet(self, playerId):
         matchData.put()
 
         # チャネルを生成
-        id = channel.create_channel(userData.user.nickname())
+#        id = channel.create_channel(userData.user.nickname())
 
         # チャネルIDをクライアントに渡す
         template_values = {'channel_id': id,
                            'player_id': playerId,
+                           'frame':matchData.frame,
                            'status': matchData.status}
         path = os.path.join(os.path.dirname(__file__), 'views', 'player.html')
         html = template.render(path, template_values)
@@ -68,10 +79,16 @@ def PlayerPagePost(self, playerId):
     matchData = MatchData.get_by_key_name('firstMatch')
     matchData.board[int(putIndex)] = {'p1':1,'p2':2}[playerId]
     matchData.status = {'p1':'p2_turn', 'p2':'p1_turn'}[playerId]
+    matchData.frame += 1;
     matchData.put()
+
+    matchData.lastTurnAction.putPlayer = playerId
+    matchData.lastTurnAction.putIndex = int(putIndex)
+    matchData.lastTurnAction.put()
 
     # 送信メッセージを作成
     messageData = {
+        'frame':matchData.frame,
         'status':matchData.status,
         'putPlayer':playerId,
         'putIndex':putIndex
@@ -80,14 +97,14 @@ def PlayerPagePost(self, playerId):
     logging.log(25, "send message :" + message)
 
     # メッセージの送信
-    if playerId == 'p1' and matchData.player2:
-        messageId = matchData.player2.user.nickname()
-        channel.send_message(messageId, message)
-        logging.log(25, "send :" + matchData.player2.user.nickname())
-    if playerId == 'p2' and matchData.player1:
-        messageId = matchData.player1.user.nickname()
-        channel.send_message(messageId, message)
-        logging.log(25, "send :" + matchData.player1.user.nickname())
+#    if playerId == 'p1' and matchData.player2:
+#        messageId = matchData.player2.user.nickname()
+#        channel.send_message(messageId, message)
+#        logging.log(25, "send :" + matchData.player2.user.nickname())
+#    if playerId == 'p2' and matchData.player1:
+#        messageId = matchData.player1.user.nickname()
+#        channel.send_message(messageId, message)
+#        logging.log(25, "send :" + matchData.player1.user.nickname())
 
     self.response.out.write(message)
 
@@ -106,8 +123,32 @@ class Player2Page(webapp.RequestHandler):
     def post(self):
         PlayerPagePost(self, 'p2')
 
+class PollingPage(webapp.RequestHandler):
+    def get(self):
+        frame = self.request.get('frame')
+        logging.log(25, "polling request:" + frame)
+
+        # 試合データの取得
+        matchName = 'firstMatch'
+        matchData = MatchData.get_by_key_name(matchName)
+
+        logging.log(25, "polling db     :" + str(matchData.frame))
+
+        # メッセージの作成
+        messageData = {'frame':matchData.frame}
+        if matchData.frame != int(frame):
+            messageData['status'] = matchData.status
+            messageData['putPlayer'] = matchData.lastTurnAction.putPlayer
+            messageData['putIndex'] = matchData.lastTurnAction.putIndex
+
+        message = simplejson.dumps(messageData)
+        logging.log(25, "polling return:" + message)
+        self.response.out.write(message)
+
+
 application = webapp.WSGIApplication([('/player1', Player1Page),
-                                      ('/player2', Player2Page)],
+                                      ('/player2', Player2Page),
+                                      ('/polling', PollingPage),],
                                       debug=True)
 
 
